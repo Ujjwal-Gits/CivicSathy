2ba23bfe7b979c38074f2b041072c8db682551c6
Ujjwal <uzalrupakheti87@gmail.com>
2026-05-20 22:10:19 +0545

    add class level documentation for EncodingFilter

--- src/main/java/com/civicsathy/filter/EncodingFilter.java
+++ src/main/java/com/civicsathy/filter/EncodingFilter.java
@@ -1,7 +1,24 @@
 package com.civicsathy.filter;
 
-public class EncodingFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import java.io.IOException;
 
+/**
+ * Utility class providing helper methods for EncodingFilter.
+ * 
+ * @author Ujjwal
+ * @version 1.0
+ */
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
 }
 
 
