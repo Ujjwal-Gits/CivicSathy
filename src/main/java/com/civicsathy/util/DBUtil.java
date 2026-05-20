3837800c00dcd3fc461fb4d460abbad47d86cb42
Ujjwal <uzalrupakheti87@gmail.com>
2026-05-20 22:10:19 +0545

    add utility documentation for DB connection methods

--- src/main/java/com/civicsathy/util/DBUtil.java
+++ src/main/java/com/civicsathy/util/DBUtil.java
@@ -4,11 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Utility class providing helper methods for DBUtil.
+ * 
+ * @author Ujjwal
+ * @version 1.0
+ */
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/civicsathy";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
+    /**
+     * Retrieves the Connection.
+     *
+     * @return The resulting Connection.
+     * @throws SQLException if an exception occurs.
+     */
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
